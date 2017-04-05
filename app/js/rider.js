8f87c59047163f1a33f81f36acd89f632daa08c1
Alex Heritier <alex.heritier@gmail.com>
2017-04-05 02:17:28 -0700

    changed some formatting

--- app/js/rider.js
+++ app/js/rider.js
@@ -14,22 +14,22 @@ app.controller("riderCtrl", function($scope, $http, userService) {
     $scope.$on('$routeChangeSuccess', function () {
         $scope.onLoad();
     });
-	
-	$scope.add = function(url){
-	$http.get(url).then(function(response) {
+
+	$scope.add = function(url) {
+        $http.get(url).then(function(response) {
             $scope.distance = response.data.row.element.distance.text;
-			$scope.time = response.data.row.element.duration.text;
+            $scope.time = response.data.row.element.duration.text;
             $scope.messages.push($scope.newMessage);
-  });
-};
+        });
+    };
+
 	ETA_BASE_URL = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial';
 	KEY = 'AIzaSyBC7wNk8LezGmsO1FLz2xH8Twz0j5VPEF8';
 	var origin = '37.335,-121.881';
 	//var destination = $scope.address;
 	var destination = '200 E Santa Clara St San Jose CA';
-	if(destination) destination.replace(/ /g, "+");
+	if (destination) destination.replace(/ /g, "+");
 	//var url = ETA_BASE_URL + '&origin='+origin+'&destination='+destination+'&key='+KEY;
 	var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=272+E+Santa+Clara+St+San+Jose+CA&destinations=1+Washington+Sq+San+Jose+CA&key=AIzaSyBC7wNk8LezGmsO1FLz2xH8Twz0j5VPEF8';
 	$scope.add(url);
-	
 });
