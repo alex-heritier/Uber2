9acd4ab1ee4fdb35ede64cbd58ea11c5d0fbe8b4
Alex Heritier <alex.heritier@gmail.com>
2017-04-12 23:48:36 -0700

    moved driver code to controller but it's buggy af

--- app/js/driver.js
+++ app/js/driver.js
@@ -1,5 +1,79 @@
 'use strict';
 
-app.controller("driverCtrl", function($scope) {
+app.controller("driverCtrl", function($scope, $http, userService) {
+    $scope.onLoad = function() {
+        var user = userService.getUser();
 
+        // check if user is an object
+        if (user == null) {
+            //document.location.href = "#/"; // go to landing
+        }
+        console.log(user);
+
+        // check if google maps is loaded (should be)
+        if (google == undefined) {
+            document.location.href = "#/"; // go to landing
+        }
+
+        $scope.initMap();
+    };
+
+    $scope.initMap = function() {
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+
+        var map = new google.maps.Map($('#map').get(), {
+            zoom: 10,
+            center: {lat: 37.3351420, lng: -121.8811}
+        });
+        directionsDisplay.setMap(map);
+        directionsDisplay.setPanel($('#right-panel').get(0));
+
+        var control = $('#floating-panel').get(0);
+        control.style.display = 'block';
+        map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        var pos;
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Location found.');
+                map.setCenter(pos);
+                map.setZoom(20);
+
+            }, function() {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+        var onChangeHandler = function() {
+            displayRoute(directionsService, directionsDisplay, pos);
+        };
+        $('#click').get(0).addEventListener('click', onChangeHandler);
+        var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);
+    }
+
+    $scope.displayRoute = function(directionsService, directionsDisplay, pos) {
+        directionsService.route({
+            origin: pos,
+            destination: {lat: 37.364018, lng: -121.929016},
+            travelMode: google.maps.TravelMode.DRIVING
+        }, function(response, status) {
+            if (status === google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+            } else {
+                window.alert('Direction request failed due to ' + status);
+            }
+        });
+    }
 });
