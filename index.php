b9dcb9500caebb4f3383c7d144d30823c2106d68
Alex Heritier <alex.heritier@gmail.com>
2017-04-20 22:53:02 -0700

    added https redirecting

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+if ($_SERVER['HTTPS'] != "on") {
+    $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    header("Location:$redirect");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
