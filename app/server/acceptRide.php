f7d09f86a242244d7f81cc7751a191771a848a13
david <davidparker241@gmail.com>
2017-04-24 19:02:30 -0700

    Bug fix attempt #3

--- app/server/acceptRide.php
+++ app/server/acceptRide.php
@@ -17,11 +17,13 @@ if ($conn->connect_error) {
 }
 
 // Update driver to busy
-$sql = "UPDATE requests SET status='pending' WHERE user_id=$user_id AND req_id=$request_id";
+$sql = "UPDATE requests SET status='in_progress' WHERE user_id=$user_id AND req_id=$request_id";
 
 if ($conn->query($sql) === TRUE) {
     echo "Record update success";
 } else {
+    echo $user_id
+    echo $request_id
     echo "Error updating record: " . $conn->error;
 }
 
