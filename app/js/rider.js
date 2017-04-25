c05d7cfd1b414a63e2d60fd24d453af475a6033c
david <davidparker241@gmail.com>
2017-04-25 16:34:25 -0700

    Fixed indentation in rider.js

--- app/js/rider.js
+++ app/js/rider.js
@@ -20,8 +20,8 @@ app.controller("riderCtrl", function($scope, $location, userService) {
     };
 
     $scope.setDestination = function() {
-	    var address = $scope.address;
-	};
+        var address = $scope.address;
+    };
 
     $scope.$on('$routeChangeSuccess', function () {
         $scope.init();
@@ -29,8 +29,8 @@ app.controller("riderCtrl", function($scope, $location, userService) {
 
     $scope.initMap = function(map) {
         $scope.map = map;
-	var trafficLayer = new google.maps.TrafficLayer();
-	trafficLayer.setMap(map);
+    var trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
         var infoWindow = new google.maps.InfoWindow({map: map});
 
         // Try HTML5 geolocation.
@@ -62,153 +62,143 @@ app.controller("riderCtrl", function($scope, $location, userService) {
         }
     };
 
-	$scope.getETA = function() {
+    $scope.getETA = function() {
         let map = $scope.map;
-    	let origin1, destinationA;
-	var check = 0;
+        let origin1, destinationA;
+        var check = 0;
 
-        if ($scope.location)
-	{
-	    check =1;
+        if ($scope.location){
+            check =1;
             origin1 = $scope.location;
-	}
-        else
-	{
-	    check = 0;
+        } else {
+            check = 0;
             origin1 = map.getCenter();
-	}
-
-    	if ($scope.destination)
-	{
-            destinationA = $scope.destination;
-	    $scope.canSubmit = true;
-	}
-    	else
-	{
-            $scope.canSubmit = false;
-            destinationA = '1 Washington Square San Jose';
-	}
-
-        let geocoder = new google.maps.Geocoder();
-	if (check ==1)
-	{
-        geocoder.geocode({'address': origin1}, function(results, status) {
-            if (status == 'OK') {
-                origin1 = results[0].geometry.location;
-                            }
-	     else {
-                console.log('Geocode was not successful for the following reason: ' + status);
-                return;
-            }
-        });
-	}
-	
-
-	geocoder.geocode({'address': destinationA}, function(results, status) {
-                    if (status == 'OK') {
-                        destinationA = results[0].geometry.location;
-			geocodingCallback();
-                    } else {
-                        console.log('Geocode was not successful for the following reason: ' + status);
-                            return;
-                    }
-                });
-
-	
-        console.log("origin1: ", origin1);
-        console.log("destinationA: ", destinationA);
-        //$scope.canSubmit = true;
-
-        function geocodingCallback() {
-        	var service = new google.maps.DistanceMatrixService();
-        	service.getDistanceMatrix({
-          	 	 origins: [origin1],
-          		 destinations: [destinationA],
-           		 travelMode: 'DRIVING',
-           		 unitSystem: google.maps.UnitSystem.IMPERIAL,
-          	}, distanceMatrixCallback);
-
-        	var directionsDisplay;
-        	var directionsService = new google.maps.DirectionsService();
-        	var request = {
-        		origin: origin1,
-        		destination: destinationA,
-        		travelMode: 'DRIVING'
-          	};
-
-         	directionsDisplay = new google.maps.DirectionsRenderer();
-            var mapOptions = {
-                zoom: 7,
-                center: origin1
-            };
-         	map = new google.maps.Map($('#map').get(0), mapOptions);
-          	directionsDisplay.setMap(map);
-		var trafficLayer = new google.maps.TrafficLayer();
-		trafficLayer.setMap(map);
-
-        	directionsService.route(request, function(result, status) {
-        		if (status == 'OK') {
-                    directionsDisplay.setDirections(result);
-                }
-          	});
         }
 
-        function distanceMatrixCallback(response, status) {
-        	if (status == 'OK') {
-        		var origins = response.originAddresses;
-        		var destinations = response.destinationAddresses;
+        if ($scope.destination){
+            destinationA = $scope.destination;
+            $scope.canSubmit = true;
+        } else {
+            $scope.canSubmit = false;
+            destinationA = '1 Washington Square San Jose';
+        }
 
-        		for (var i = 0; i < origins.length; i++) {
-          			var results = response.rows[i].elements;
-          			for (var j = 0; j < results.length; j++) {
-            			var element = results[j];
-            			$scope.distance = element.distance.text;
-            			$scope.duration = element.duration.text;
-        				var cost = element.distance.value * 0.002;
-        				cost = parseFloat(Math.round(cost * 100) / 100).toFixed(2);
-        				$scope.cost = cost;
-            			var from = origins[i];
-            			var to = destinations[j];
-      				}
-    			}
-      		}
-    	}
-	};
-
-    $scope.requestRide = function() {
         let geocoder = new google.maps.Geocoder();
-	var check = 0;
-	if($scope.location)
-	{
-        	var location = $scope.location;
-		check = 1;
-	}
-	else {
-		location = $scope.map.getCenter();
-		check = 0;
-	}
+        
+        if (check ==1){
+            geocoder.geocode({'address': origin1}, function(results, status) {
+                if (status == 'OK') {
+                    origin1 = results[0].geometry.location;
+                } else {
+                    console.log('Geocode was not successful for the following reason: ' + status);
+                    return;
+                }
+            });
+        }
+    
 
-        if (!$scope.canSubmit) { alert("Invalid location"); return; }
-
-        console.log(location);
-	if(check ==1)
-	{
-        geocoder.geocode({'address': location}, function(results, status) {
+        geocoder.geocode({'address': destinationA}, function(results, status) {
             if (status == 'OK') {
-                location = results[0].geometry.location;
-                console.log(location);
-                 
+                destinationA = results[0].geometry.location;
+                geocodingCallback();
             } else {
                 console.log('Geocode was not successful for the following reason: ' + status);
                 return;
             }
         });
-	}
-	$.post(window.root + "app/server/request.php",
-                    {email: $scope.user.email, lat: location.lat, lng: location.lng},
-                    function(data) {
-                        console.log(data);
+
+    
+        console.log("origin1: ", origin1);
+        console.log("destinationA: ", destinationA);
+        //$scope.canSubmit = true;
+
+        function geocodingCallback() {
+            var service = new google.maps.DistanceMatrixService();
+            service.getDistanceMatrix({
+                 origins: [origin1],
+                 destinations: [destinationA],
+                 travelMode: 'DRIVING',
+                 unitSystem: google.maps.UnitSystem.IMPERIAL,
+            }, distanceMatrixCallback);
+
+            var directionsDisplay;
+            var directionsService = new google.maps.DirectionsService();
+            var request = {
+                origin: origin1,
+                destination: destinationA,
+                travelMode: 'DRIVING'
+            };
+
+            directionsDisplay = new google.maps.DirectionsRenderer();
+            var mapOptions = {
+                zoom: 7,
+                center: origin1
+            };
+            map = new google.maps.Map($('#map').get(0), mapOptions);
+            directionsDisplay.setMap(map);
+            var trafficLayer = new google.maps.TrafficLayer();
+            trafficLayer.setMap(map);
+
+            directionsService.route(request, function(result, status) {
+                if (status == 'OK') {
+                    directionsDisplay.setDirections(result);
+                }
+            });
+        }
+
+        function distanceMatrixCallback(response, status) {
+            if (status == 'OK') {
+                var origins = response.originAddresses;
+                var destinations = response.destinationAddresses;
+
+                for (var i = 0; i < origins.length; i++) {
+                    var results = response.rows[i].elements;
+                    for (var j = 0; j < results.length; j++) {
+                        var element = results[j];
+                        $scope.distance = element.distance.text;
+                        $scope.duration = element.duration.text;
+                        var cost = element.distance.value * 0.002;
+                        cost = parseFloat(Math.round(cost * 100) / 100).toFixed(2);
+                        $scope.cost = cost;
+                        var from = origins[i];
+                        var to = destinations[j];
                     }
+                }
+            }
+        }
+    };
+
+    $scope.requestRide = function() {
+        let geocoder = new google.maps.Geocoder();
+        var check = 0;
+        if($scope.location){
+                var location = $scope.location;
+            check = 1;
+        } else {
+            location = $scope.map.getCenter();
+            check = 0;
+        }
+
+        if (!$scope.canSubmit) { alert("Invalid location"); return; }
+
+        console.log(location);
+        if(check ==1){
+            geocoder.geocode({'address': location}, function(results, status) {
+                if (status == 'OK') {
+                    location = results[0].geometry.location;
+                    console.log(location);
+                     
+                } else {
+                    console.log('Geocode was not successful for the following reason: ' + status);
+                    return;
+                }
+            });
+        }
+        $.post(window.root + "app/server/request.php",
+            {email: $scope.user.email, lat: location.lat, lng: location.lng},
+            function(data) {
+                console.log(data);
+            }
         );
     };
 
