3957eb1f8da07314c142fb7446a66090881e1587
david <davidparker241@gmail.com>
2017-05-12 17:00:58 -0700

    Fix for accept button not updating page (Attempt #8)

--- app/js/driver.js
+++ app/js/driver.js
@@ -40,6 +40,7 @@ app.controller("driverCtrl", function($scope, $rootScope, $location, $http, user
                 console.log(data);
             }
         );
+        $scope.available = false;
         $scope.setRequests();
     };
 
